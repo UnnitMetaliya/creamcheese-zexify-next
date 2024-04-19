f868c04a0c18e53e039b776b9e135ebdb924bb91
Unnit Metaliya <unnitmetaliya@unnits-mbp.home>
2024-04-18 23:11:54 -0400

    mode toggle is working fine

--- src/components/site/navigation/index.tsx
+++ src/components/site/navigation/index.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from "@/components/global/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import { User } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
@@ -34,6 +36,8 @@ const Navigation = ({ user }: Props) => {
         >
           Login
         </Link>
+        <UserButton />
+        <ModeToggle />
       </aside>
     </div>
   );
